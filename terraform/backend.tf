a9ec004532bc88970e317ffd27d1ed8de0cb3d00
jani12 <jani.mqw0531@gmai.com>
2024-10-04 21:41:16 +0100

    intial commit1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-github-oidc-bucket-eks"
+    bucket = "my-github-oidc-bucket-12qw"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
   }
