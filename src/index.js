afe4c66557c3fd910c1e15cdfb3e001beb29b5a1
jani12 <jani.mqw0531@gmai.com>
2024-10-16 14:28:33 +0100

    modifiying code chnages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var os = require('os')
 var http = require('http')
 
 function handleRequest(req, res) {
-  res.write('Hi there! I\'m being served from newwly ' + os.hostname())
+  res.write('Hi there! I\'m being served from newwly testing purose' + os.hostname())
   res.end()
 }
 http.createServer(handleRequest).listen(3000)
